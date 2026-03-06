79d9d25e8496b236be9ea3051b7643e04213e197
Druid-alpha <oluwafemiajayi103@gmail.com>
2026-03-06 10:46:47 +0100

    added import for trash2

--- src/pages/admin/ProductForm.jsx
+++ src/pages/admin/ProductForm.jsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 import { useToast } from '@/hooks/use-toast'
+import { Trash2 } from 'lucide-react'
 import axios from '@/lib/axios'
 import {
   useCreateProductMutation,
