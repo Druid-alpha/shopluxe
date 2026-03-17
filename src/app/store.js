169b92773f37c505af638bd9ca49c67386497276
Druid-alpha <oluwafemiajayi103@gmail.com>
2026-03-17 22:25:34 +0100

    fix store issue

--- src/app/store.js
+++ src/app/store.js
@@ -3,14 +3,14 @@ import { api } from "./api";
 
 import authReducer from "@/features/auth/authSlice";
 import cartReducer from "@/features/cart/cartSlice";
-// import wishlistReducer from "@/features/wishlist/wishListSlice";
+import wishlistReducer from "@/features/wishlist/wishlistSlice";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth: authReducer,
     cart: cartReducer,
-    // wishlist: wishlistReducer
+    wishlist: wishlistReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
